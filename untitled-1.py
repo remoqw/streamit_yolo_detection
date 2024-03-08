8b57c7bba424e1e1eba9bd6fa3b438e7950bd4b9
nikeee7 <158345288+nikeee7@users.noreply.github.com>
2024-03-08 12:57:14 +0300

    Add files via upload

---
+++ untitled-1.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import torch
+from ultralytics import YOLO
+from PIL import Image
+import os
+
+if torch.cuda.is_available(): device = 'cuda'
+else: device = 'cpu'
+
+model = YOLO(r"runs\detect\train4\weights\best.pt")
+
+uploaded_files = st.file_uploader("Выберите изображения", accept_multiple_files=True)
+
+
+if st.button("Справка"):
+    st.write('hello')
+if st.button("Скрыть справку"):
+    st.write('')
+
+
+for file in uploaded_files:
+    img = Image.open(file)
+    pred = model.predict(img)[0]
+    pred.save(file.name)
+    filename = file.name[:file.name.rfind(".")]+'.txt'
+    open(filename, 'a')
+    if filename in os.listdir("qwerty"):
+        os.remove("qwerty/"+filename)
+    pred.save_txt('qwerty/'+filename)
+    st.image(file.name, caption=file.name)
+    st.download_button(
+        label = "Скачать",
+        data = open(filename, 'r'),
+        file_name = filename
+    )
+    os.remove(filename)
+    os.remove(file.name)
